9e8b80b46d67cf82af822b8caaccfef5abce8588
Ikbel <ikbel.kira@live.fr>
2022-06-19 19:59:57 +0100

    Add authentication with a secret key

--- src/routes/handleJob.js
+++ src/routes/handleJob.js
@@ -1,9 +1,16 @@
 import { queue } from "../queue.js";
+import config from "../config.js";
 
 queue.process();
 
-export default async function handleJob(req) {
-  const { csvFileUrl, callbackUrl } = req.body;
+export default async function handleJob(req, res) {
+  const { csvFileUrl, callbackUrl, secretKey } = req.body;
+
+  if (secretKey !== config.queue.secretKey) {
+    return res.code(401).send({
+      error: "Invalid secret key",
+    });
+  }
 
   // Create a new job
   await queue.add({
